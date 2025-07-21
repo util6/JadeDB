d546fa345000d8cc828efeef2eb364164d4ca442
util6 <958532148@qq.com>
2025-07-21 12:43:10 +0800

    chore:将corekv2.0 新增的文件通过文件覆盖的方法合并过来 添加 .gitignore并创建 SpecStory 说明文档- 新增 .gitignore 文件，排除特定目录和文件
    - 创建 .specstory/.what-is-this.md，介绍 SpecStory 艺术ifact目录的用途和功能

---
+++ fuzz_test.go
@@ -0,0 +1,46 @@
+package corekv
+
+import (
+	"github.com/hardcore-os/corekv/utils"
+
+	"testing"
+	"time"
+)
+
+func FuzzAPI(f *testing.F) {
+	//添加种子语料，必须和闭包西数的模弼参数一一对应
+	f.Add([]byte("core"), []byte("kv"))
+	clearDir()
+	db, _ := Open(opt)
+
+	defer func() { _ = db.Close() }()
+	//运行fuz2引学，不断生成测试用例进行测试
+	f.Fuzz(func(t *testing.T, key, value []byte) {
+
+		e := utils.NewEntry(key, value).WithTTL(1 * time.Second)
+		if err := db.Set(e); err != nil {
+			t.Fatal(err)
+		}
+
+		//道询
+		if entry, err := db.Get(key); err != nil {
+			t.Fatal(err)
+		} else {
+			t.Logf("db.Get key=%s,value=%s,expiresAt=%d", entry.Key, entry.Value, entry.ExpiresAt)
+		}
+		iter := db.NewIterator(&utils.Options{
+			IsAsc: false,
+		})
+		defer func() { _ = iter.Close() }()
+		for iter.Rewind(); iter.Valid(); iter.Next() {
+			it := iter.Item()
+			t.Logf("db.NewIterator key=%s,value=%s,expiresAt=%d", it.Entry().Key, it.Entry().Value, it.Entry().ExpiresAt)
+		}
+		t.Logf("db.stats.EntryNum=%+v", db.Info().EntryNum)
+		//除
+		if err := db.Del(key); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+}
